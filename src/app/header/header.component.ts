9a887afadf8b60f47ce0c8e1c022c3e8bd444831
Thasneem Fareed <thasneemfareed@Thasneems-MacBook-Pro.local>
2023-05-20 10:39:19 +0530

    navigate the page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router'; 
+
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) {
+
+  }
 
   ngOnInit() {
   }
 
+  navigateToContent() {
+    this.router.navigate(['content']);
+  }
 }
