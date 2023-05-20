aa55af42329201ea3b108dee7ef91085618989b4
Thasneem Fareed <thasneemfareed@Thasneems-MacBook-Pro.local>
2023-05-20 10:02:44 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'header', component: HeaderComponent },
+  { path: 'content', component: ContentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HeaderComponent, ContentComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
